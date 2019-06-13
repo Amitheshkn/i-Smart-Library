89a8e0be6e5300ebf019cb58e4a63b3855e2d1f8
AMITHESH K N <46914484+Amitheshkn@users.noreply.github.com>
2019-06-14 00:25:59 +0530

    Update viewfeedback.php

--- viewfeedback.php
+++ viewfeedback.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bike-service";
+$dbname = "ismartlibrary";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
